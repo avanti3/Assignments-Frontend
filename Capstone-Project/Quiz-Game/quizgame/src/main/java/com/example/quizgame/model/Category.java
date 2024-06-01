fea06fafb7ce7dda44150c7e3ec3dd5f5045d9fc
avanti <avantigupta3@gmail.com>
2024-06-01 13:14:14 +0530

    new-changes

--- Capstone-Project/Quiz-Game/quizgame/src/main/java/com/example/quizgame/model/Category.java
+++ Capstone-Project/Quiz-Game/quizgame/src/main/java/com/example/quizgame/model/Category.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 
 
 
@@ -13,9 +12,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
-    
-    
-    @NotBlank(message = "Category name is mandatory")
+
     private String name;
 
     
